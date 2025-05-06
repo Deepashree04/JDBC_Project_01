b115e7ba2b22fdd15b767e5376c3fff84e89e97e
Deepashree04 <151150466+Deepashree04@users.noreply.github.com>
2025-05-06 09:31:40 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -4,7 +4,12 @@ public class Program1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		//System.out.println("Hello world!");
+		System.out.println("Enter the num:");
+		int n=sc.nextInt();
+		int square=n*n;
+		System.out.println("Square of the num "+n+"is: "+square);
+
 
 	}
 
